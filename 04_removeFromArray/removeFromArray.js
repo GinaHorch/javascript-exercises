895bb2b5881aa3a3d27206d342f04a486fe712fe
Gina Horch <168549659+GinaHorch@users.noreply.github.com>
2024-06-24 19:37:22 +0800

    Odin JavaScript exercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removed) {
+    return array.filter(item => !removed.includes(item));
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
+console.log(removeFromArray([1, 2, 2, 3], 2));
+console.log(removeFromArray([1,2,3,4], 1, 2, 3, 4));
+console.log(removeFromArray(["hey", 2, 3, "ho"]), "hey", 3);
+console.log(removeFromArray([1, 2, 3], "1", 3))
 // Do not edit below this line
 module.exports = removeFromArray;
