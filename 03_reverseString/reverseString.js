562e8e50b7f542cfacb4ad93241c4444ec8800ba
Gina Horch <168549659+GinaHorch@users.noreply.github.com>
2024-06-24 13:04:01 +0800

    Odin JavaScript exercises 01 - 03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reversed = '';
+  for (let i = string.length - 1; i >=0; i--) {
+    reversed += string[i];
+  }
+  return reversed;
 };
+console.log(reverseString("hello"));
+console.log(reverseString("hello there"));
+console.log(reverseString('123! abc! Hello, Odinite.'));
+console.log(reverseString(''));
 
 // Do not edit below this line
 module.exports = reverseString;
