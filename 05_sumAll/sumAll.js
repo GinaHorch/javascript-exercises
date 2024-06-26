8d3ec1550dcd336710d6b573f62399e020aa8e92
Gina Horch <168549659+GinaHorch@users.noreply.github.com>
2024-06-26 18:16:24 +0800

    Completed Odin JavaScript exercise 05 sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (start > end) {
+        [start, end] = [end, start];
+    } if (start < 0 || end < 0 || typeof start !== 'number' || typeof end !== 'number') {
+        return "ERROR";
+    }
+    for (let i = start; i <= end; i++) {
+     sum += i;
+    }
+    return sum;
+ }
 
-};
+ console.log(sumAll(1, 4));
+ console.log(sumAll(1, 4000));
+ console.log(sumAll(123, 1));
+ console.log(sumAll(-10, 4));
+ console.log(sumAll(10, "90"));
+ console.log(sumAll(10, [90, 1]));
 
 // Do not edit below this line
 module.exports = sumAll;
