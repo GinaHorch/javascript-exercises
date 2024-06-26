ed6fa5a649db663209d839c6a94dfb5a89c37a5e
Gina Horch <168549659+GinaHorch@users.noreply.github.com>
2024-06-26 19:56:55 +0800

    Completed Odin JavaScript-exercise 06 leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (typeof year !== 'number' || !Number.isInteger(year)){
+        return "ERROR";
+    }
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
 
-};
+console.log(leapYears(1996));
+console.log(leapYears(1997));
+console.log(leapYears(34992));
+console.log(leapYears(1900));
+console.log(leapYears(1600));
+console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
