c02f37892836b111ff8c36d57250d6a4a974595f
Gina Horch <168549659+GinaHorch@users.noreply.github.com>
2024-07-06 20:49:02 +0800

    Completed 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    return cleanedStr === cleanedStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
